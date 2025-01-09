5864b0e06b8cb8421ef1220a645774671a0f3e8a
d7meDev <d7me446@gmail.com>
2025-01-09 15:43:36 +0300

    clousres

--- counter.js
+++ counter.js
@@ -1,21 +1,25 @@
 const counter = document.querySelector(".header .counter h1");
 function Counter(){
-   const startTime = Date.now();
-   const counterID = setInterval(updateTime,1); 
-
-
-   function updateTime(){
-    const currentTime = new Date(Date.now() - startTime);
+   function startCounter(){
+      const startTime = Date.now();
+      const counterID = setInterval(updateTime,1);
+      function updateTime(){
+         const currentTime = new Date(Date.now() - startTime);
+          
+          counter.textContent = `${currentTime.getMinutes()}:${currentTime.getSeconds().toString().padStart(2,"0")}`;
      
-     counter.textContent = `${currentTime.getMinutes()}:${currentTime.getSeconds().toString().padStart(2,"0")}`;
-
+        }
    }
+    
+
+
+   
 
    function stopCounter(){
       clearInterval(counterID);
    }
 
-   return stopCounter;
+   return {startCounter,stopCounter};
 }
 
-export {Counter}
+export {Counter};
