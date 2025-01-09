8e92874bd3940750ae954bcb5ad34a13dd7cd054
d7meDev <d7me446@gmail.com>
2025-01-09 17:42:02 +0300

    a lot

---
+++ wordsGenreator.js
@@ -0,0 +1,44 @@
+
+async function genreateWord() {
+let word = "";
+let succses = false;
+while(!succses){
+    try {
+        const res = await fetch(`https://random-word-api.vercel.app/api?words=1&length=${(Math.floor(Math.random() * 7)) + 1}`);
+        if(!res.ok){
+            throw new Error("rseponse did not succeed")
+        }
+        const data = await res.json();
+        [word] = data;
+
+        succses = true;
+        
+    }
+
+    catch(error){
+        
+        console.error("try again!")
+    }
+}
+
+  return word;
+    
+}
+async function getWord(){
+    const wordBox = document.createElement("div");
+    wordBox.classList.add("word-box")
+    let postion = Math.floor((Math.random() * 71)) + 20;
+    wordBox.style.left= `${postion}%`
+    wordBox.style.transform = `trasnlateX(${postion})`
+    let word = await genreateWord();
+    word = word.charAt(0).toUpperCase() + word.substring(1);
+    wordBox.innerHTML = `<h3>${word}</h3>
+                        <img src="Bubble.png" alt="">`;
+    return wordBox;
+}
+
+setInterval(async() => {
+    let word = await getWord();
+document.body.appendChild(word)
+},5000)
+
